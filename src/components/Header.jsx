6d47ae781448a40b197f0475515215c2e332476f
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-10-19 09:43:08 +0530

    feat: add header component with an icon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,14 @@
+import Icon from '@mdi/react';
+import { mdiFileAccount } from '@mdi/js';
+
 function Header() {
     return (
         <header>
-            <h1>CV Application</h1>
+            <h1>
+                <Icon path={mdiFileAccount} size={1} /> CV Application
+            </h1>
         </header>
-    )
+    );
 }
 
 export default Header;
