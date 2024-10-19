add82535420eae338c71c1efecfaaacb10823529
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-10-19 10:12:49 +0530

    feat: create and setup footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+function Footer() {
+    return (
+        <footer>
+            <div>
+                <p>Copyright &copy; Abdullah Zeeshan 2024</p>
+            </div>
+            <div>
+                <a href="https://github.com/4bdullah7eeshan" target="_blank" rel="noopener noreferrer">GitHub</a>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
