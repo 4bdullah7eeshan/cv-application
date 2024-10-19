45b8fe89a4222fa71035fff0a48106e128b761f8
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-10-19 14:00:41 +0530

    feat: improve resume content layout

--- src/components/CVPreview.jsx
+++ src/components/CVPreview.jsx
@@ -3,23 +3,25 @@ function CVPreview({ userData }) {
 
     return (
         <section className="cv-preview">
-            <h2>CV</h2>
-            
-            <div>
-                <h3>General Information</h3>
-                <p><strong>Name:</strong> {name}</p>
-                <p><strong>Email:</strong> {email}</p>
-                <p><strong>Phone:</strong> {phone}</p>
-            </div>
+            <div className="resume-header">
+                <h1>{name}</h1>
+                <div className="resume-contact-info">
+                    <p>{email}</p>
+                    <p>{phone}</p>
+                </div>
 
-            <div>
-                <h3>Educational Experience</h3>
+            </div>
+            <hr />
+            <div className="resume-education-section">
+                <h3>EDUCATION</h3>
                 {education.length > 0 ? (
                     education.map((edu, index) => (
                         <div key={index}>
-                            <p><strong>School Name:</strong> {edu.school}</p>
-                            <p><strong>Title of Study:</strong> {edu.title}</p>
-                            <p><strong>Date of Study:</strong> {edu.date}</p>
+                            <div className="fr">
+                                <p><strong>{edu.school}</strong></p>
+                                <p>{edu.date}</p>
+                            </div>
+                            <p><em>{edu.title}</em></p>
                         </div>
                     ))
                 ) : (
@@ -27,16 +29,17 @@ function CVPreview({ userData }) {
                 )}
             </div>
 
-            <div>
-                <h3>Practical Experience</h3>
+            <div className="resume-experience-section">
+                <h3>EXPERIENCE</h3>
                 {experience.length > 0 ? (
                     experience.map((exp, index) => (
                         <div key={index}>
-                            <p><strong>Company Name:</strong> {exp.company}</p>
-                            <p><strong>Position Title:</strong> {exp.position}</p>
+                            <div className="fr">
+                                <p><strong>{exp.company}</strong></p>
+                                <p>{exp.fromDate} - {exp.toDate}</p>
+                            </div>
+                            <p><em>{exp.position}</em></p>
                             <p><strong>Responsibilities:</strong> {exp.responsibilities}</p>
-                            <p><strong>Date From:</strong> {exp.fromDate}</p>
-                            <p><strong>Date Until:</strong> {exp.toDate}</p>
                         </div>
                     ))
                 ) : (
