ea375f39c3e6cc3e095a0d6d154cc62211535908
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-10-19 10:22:11 +0530

    fix(github icon): install and use correct package

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,5 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGithub } from '@fortawesome/free-solid-svg-icons';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
 function Footer() {
     return (
