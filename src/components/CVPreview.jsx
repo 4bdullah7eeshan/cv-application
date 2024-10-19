268634bdeb614febb8f0aefb632aab7a4f0b7f1c
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-10-19 12:27:15 +0530

    wip(cv): setup cv preview data

--- src/components/CVPreview.jsx
+++ src/components/CVPreview.jsx
@@ -1,7 +1,14 @@
-function CVPreview() {
+function CVPreview({ userData }) {
+    const { name, email, phone, education, experience } = userData;
     return (
         <section className="cv-preview">
             <h2>CV</h2>
+            <div>
+                <h3>General Information</h3>
+                <p>Name: {name}</p>
+                <p>Email: {email}</p>
+                <p>Phone: {phone}</p>
+            </div>
         </section>
     );
 }
