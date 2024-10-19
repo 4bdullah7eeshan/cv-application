4054d4c44256470e64e8362f31315e06203f6a31
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-10-19 10:09:33 +0530

    feat: import UserInput and CVPreview in Main comp

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,11 @@
+import UserInput from "./UserInput";
+import CVPreview from "./CVPreview";
+
 function Main() {
     return (
         <main>
-
+            <UserInput />
+            <CVPreview />
         </main>
     );
 }
